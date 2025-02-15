2fff0fa756d28dc79767b4a38418bc5ef566b7d1
PRASENJITSWIN10\prasenjits <Prasenjits@mindfiresolutions.com>
2025-02-15 23:37:57 +0530

    Added tc -1

--- JenkinsSeleniumAutomationProject/src/main/java/testBase/TestBase.java
+++ JenkinsSeleniumAutomationProject/src/main/java/testBase/TestBase.java
@@ -1,9 +1,20 @@
 package testBase;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class TestBase 
 {
-	public static WebDriver driver = new FirefoxDriver();
+	public static WebDriver driver = null ;
+	
+	public static String url = "https://opensource-demo.orangehrmlive.com/";
+	
+	public static String usernameInputBox_css = "input[name='username']";
+	public static String passwordInputBox_css = "input[name='password']";
+	public static String loginButton_xpath = "//button[@type='submit']";
+	public static String dashboard_xpath = "//h6[text()='Dashboard']";
+	public static String dashboarddd_css = "span.oxd-userdropdown-tab>i";
+	public static String logout_xpath = "//a[text()='Logout']";
+	
+	
 }
--- JenkinsSeleniumAutomationProject/src/test/java/regressionTestCases/RegressionTest.java
+++ JenkinsSeleniumAutomationProject/src/test/java/regressionTestCases/RegressionTest.java
@@ -1,16 +1,55 @@
 package regressionTestCases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import testBase.TestBase;
 
 public class RegressionTest extends TestBase
 {
+	public static WebDriver driver = new FirefoxDriver();
+	
+	public static String correctUsername = "Admin";
+	public static String correctPassword  = "admin123";
 
-	@Test
-	public void test()
+	
+	@Test(groups= {"Regression_TC_001"})
+	public void verify_login_is_successful_with_correct_credentials() throws InterruptedException
 	{
-		driver .get("https://www.google.com");
+		System.out.println("Browser Launched");
+		
+		driver.get(url);
+		System.out.println("Navigated to url : "+url);
+		
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(usernameInputBox_css)).sendKeys(correctUsername);
+		System.out.println("Username entered is : "+correctUsername);
+		
+		driver.findElement(By.cssSelector(passwordInputBox_css)).sendKeys(correctPassword);
+		System.out.println("Password entered is : "+correctPassword);
+		
+		driver.findElement(By.xpath(loginButton_xpath)).click();
+		System.out.println("Login button is clicked");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(dashboard_xpath)).isDisplayed();
+		System.out.println("Dashboard is displayed");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(dashboarddd_css)).click();
+		System.out.println("Drop Down option button is clicked");
+		
+		driver.findElement(By.xpath(logout_xpath)).click();
+		System.out.println("Logout option is clicked");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(loginButton_xpath)).isDisplayed();
+		System.out.println("Login button is Displayed");
+		
+		driver.quit();
+		System.out.println("Browser Quited");
 	}
 	
 }
